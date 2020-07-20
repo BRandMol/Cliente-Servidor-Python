861c2be548cc601ae6f22f782311a30685df56a7
ichthys721 <68262674+ichthys721@users.noreply.github.com>
2020-07-20 12:00:26 -0400

    Update servidor.py
    Funcionalidad que convierte las unidades de medida

--- servidor.py
+++ servidor.py
@@ -1,5 +1,24 @@
 import socket
 
+def convert(a, b, c):
+    values = {
+        "km": 1000.0,
+        "m": 1.0,
+        "dm": 0.1,
+        "cm": 0.01,
+        "mm": 0.001,
+        "micron": 0.000001,
+        "nm": 0.00000001,
+        "mi": 1609.34,
+        "yd": 0.9144,
+        "ft": 0.3048,
+        "in":0.0254,
+        "Nm": 1852.0
+    }
+
+    return (values[b] * a) / values[c]
+
+
 ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ser.bind(('192.168.122.214', 9090))
@@ -15,7 +34,7 @@ while True:
 	print("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]))
 
 	data= recibido.decode("utf-8").split()
-	msg_toSend = "Recibi tu mensaje"
+	msg_toSend = str(convert(int(data[0]),data[1],data[3]))
 
 	cli.send(msg_toSend.encode('ascii'))
 
